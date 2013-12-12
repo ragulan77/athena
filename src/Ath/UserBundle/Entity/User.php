cadf6015ec1c2d2e1ec65e2bb97a6ddb7fe347e8
ragulan77 <ragulan.charlesm@gmail.com>
2013-12-12 21:53:36 +0100

    ajout role pour utilisateur

--- src/Ath/UserBundle/Entity/User.php
+++ src/Ath/UserBundle/Entity/User.php
@@ -6,268 +6,286 @@ use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Ath\UserBundle\Entity\User
- *
- * @ORM\Table()
- * @ORM\Entity(repositoryClass="Ath\UserBundle\Entity\UserRepository")
- */
+* Ath\UserBundle\Entity\User
+*
+* @ORM\Table()
+* @ORM\Entity(repositoryClass="Ath\UserBundle\Entity\UserRepository")
+*/
 class User extends BaseUser
 {
-    /**
-     * @var integer $id
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
+  /**
+   * @var integer $id
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
 
-    /**
-     * @var string $firstname
-     *
-     * @ORM\Column(name="firstname", type="string", length=40)
-     */
-    private $firstname;
+  /**
+   * @var string $firstname
+   *
+   * @ORM\Column(name="firstname", type="string", length=40)
+   */
+  private $firstname;
 
-    /**
-     * @var string $lastname
-     *
-     * @ORM\Column(name="lastname", type="string", length=50)
-     */
-    private $lastname;
+  /**
+   * @var string $lastname
+   *
+   * @ORM\Column(name="lastname", type="string", length=50)
+   */
+  private $lastname;
 
-    /**
-     * @var date $birthdate
-     *
-     * @ORM\Column(name="birthdate", type="date", nullable=true)
-     */
-    private $birthdate;
-
-
-    /**
-     * @var string $phone
-     *
-     * @ORM\Column(name="phone", type="string", length=10, nullable=true)
-     */
-    private $phone;
-
-    /**
-     * @var string $address
-     *
-     * @ORM\Column(name="address", type="string", length=255, nullable=true)
-     */
-    private $address;
-
-    /**
-     * @var string $zipcode
-     *
-     * @ORM\Column(name="zipcode", type="string", length=5, nullable=true)
-     */
-    private $zipcode;
-
-    /**
-     * @var string $city
-     *
-     * @ORM\Column(name="city", type="string", length=40, nullable=true)
-     */
-    private $city;
-
-    /**
-     * @var string $country
-     *
-     * @ORM\Column(name="country", type="string", length=3, nullable=true)
-     */
-    private $country;
+  /**
+   * @var date $birthdate
+   *
+   * @ORM\Column(name="birthdate", type="date", nullable=true)
+   */
+  private $birthdate;
 
 
-    public function __construct()
-    {
-        parent::__construct();
+  /**
+   * @var string $phone
+   *
+   * @ORM\Column(name="phone", type="string", length=10, nullable=true)
+   */
+  private $phone;
 
-        $suscribeDate = new \Datetime();
-        $birthdate = new \Datetime();
-    }
+  /**
+   * @var string $address
+   *
+   * @ORM\Column(name="address", type="string", length=255, nullable=true)
+   */
+  private $address;
 
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+  /**
+   * @var string $zipcode
+   *
+   * @ORM\Column(name="zipcode", type="string", length=5, nullable=true)
+   */
+  private $zipcode;
 
-    /**
-     * Set firstname
-     *
-     * @param string $firstname
-     */
-    public function setFirstname($firstname)
-    {
-        $this->firstname = $firstname;
-    }
+  /**
+   * @var string $city
+   *
+   * @ORM\Column(name="city", type="string", length=40, nullable=true)
+   */
+  private $city;
 
-    /**
-     * Get firstname
-     *
-     * @return string
-     */
-    public function getFirstname()
-    {
-        return $this->firstname;
-    }
+  /**
+   * @var string $country
+   *
+   * @ORM\Column(name="country", type="string", length=3, nullable=true)
+   */
+  private $country;
 
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return ucfirst($this->firstname) . " " . ucfirst($this->lastname);
-    }
+  /**
+   * @ORM\Column(name="roles", type="array")
+   */
+  private $roles;
 
 
-    /**
-     * Set lastname
-     *
-     * @param string $lastname
-     */
-    public function setLastname($lastname)
-    {
-        $this->lastname = $lastname;
-    }
+  public function __construct()
+  {
+    parent::__construct();
 
-    /**
-     * Get lastname
-     *
-     * @return string
-     */
-    public function getLastname()
-    {
-        return $this->lastname;
-    }
+    $suscribeDate = new \Datetime();
+    $birthdate = new \Datetime();
+    $thid->roles = array();
+  }
+
+  /**
+   * Get id
+   *
+   * @return integer
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * Set firstname
+   *
+   * @param string $firstname
+   */
+  public function setFirstname($firstname)
+  {
+    $this->firstname = $firstname;
+  }
+
+  /**
+   * Get firstname
+   *
+   * @return string
+   */
+  public function getFirstname()
+  {
+    return $this->firstname;
+  }
 
 
-    /**
-     * Set birthdate
-     *
-     * @param date $birthdate
-     */
-    public function setBirthdate($birthdate)
-    {
-        $this->birthdate = $birthdate;
-    }
+  /**
+   * Get name
+   *
+   * @return string
+   */
+  public function getName()
+  {
+    return ucfirst($this->firstname) . " " . ucfirst($this->lastname);
+  }
 
-    /**
-     * Get birthdate
-     *
-     * @return date
-     */
-    public function getBirthdate()
-    {
-        return $this->birthdate;
-    }
 
-    /**
-     * Set phone
-     *
-     * @param string $phone
-     */
-    public function setPhone($phone)
-    {
-        $this->phone = $phone;
-    }
+  /**
+   * Set lastname
+   *
+   * @param string $lastname
+   */
+  public function setLastname($lastname)
+  {
+    $this->lastname = $lastname;
+  }
 
-    /**
-     * Get phone
-     *
-     * @return string
-     */
-    public function getPhone()
-    {
-        return $this->phone;
-    }
+  /**
+   * Get lastname
+   *
+   * @return string
+   */
+  public function getLastname()
+  {
+    return $this->lastname;
+  }
 
-    /**
-     * Set address
-     *
-     * @param string $address
-     */
-    public function setAddress($address)
-    {
-        $this->address = $address;
-    }
 
-    /**
-     * Get address
-     *
-     * @return string
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
+  /**
+   * Set birthdate
+   *
+   * @param date $birthdate
+   */
+  public function setBirthdate($birthdate)
+  {
+    $this->birthdate = $birthdate;
+  }
 
-    /**
-     * Set zipcode
-     *
-     * @param string $zipcode
-     */
-    public function setZipcode($zipcode)
-    {
-        $this->zipcode = $zipcode;
-    }
+  /**
+   * Get birthdate
+   *
+   * @return date
+   */
+  public function getBirthdate()
+  {
+    return $this->birthdate;
+  }
 
-    /**
-     * Get zipcode
-     *
-     * @return string
-     */
-    public function getZipcode()
-    {
-        return $this->zipcode;
-    }
+  /**
+   * Set phone
+   *
+   * @param string $phone
+   */
+  public function setPhone($phone)
+  {
+    $this->phone = $phone;
+  }
 
-    /**
-     * Set city
-     *
-     * @param string $city
-     */
-    public function setCity($city)
-    {
-        $this->city = $city;
-    }
+  /**
+   * Get phone
+   *
+   * @return string
+   */
+  public function getPhone()
+  {
+    return $this->phone;
+  }
 
-    /**
-     * Get city
-     *
-     * @return string
-     */
-    public function getCity()
-    {
-        return $this->city;
-    }
+  /**
+   * Set address
+   *
+   * @param string $address
+   */
+  public function setAddress($address)
+  {
+    $this->address = $address;
+  }
 
-    /**
-     * Set country
-     *
-     * @param string $country
-     */
-    public function setCountry($country)
-    {
-        $this->country = $country;
-    }
+  /**
+   * Get address
+   *
+   * @return string
+   */
+  public function getAddress()
+  {
+    return $this->address;
+  }
 
-    /**
-     * Get country
-     *
-     * @return string
-     */
-    public function getCountry()
-    {
-        return $this->country;
-    }
+  /**
+   * Set zipcode
+   *
+   * @param string $zipcode
+   */
+  public function setZipcode($zipcode)
+  {
+    $this->zipcode = $zipcode;
+  }
+
+  /**
+   * Get zipcode
+   *
+   * @return string
+   */
+  public function getZipcode()
+  {
+    return $this->zipcode;
+  }
+
+  /**
+   * Set city
+   *
+   * @param string $city
+   */
+  public function setCity($city)
+  {
+    $this->city = $city;
+  }
+
+  /**
+   * Get city
+   *
+   * @return string
+   */
+  public function getCity()
+  {
+    return $this->city;
+  }
+
+  /**
+   * Set country
+   *
+   * @param string $country
+   */
+  public function setCountry($country)
+  {
+    $this->country = $country;
+  }
+
+  /**
+   * Get country
+   *
+   * @return string
+   */
+  public function getCountry()
+  {
+      return $this->country;
+  }
+
+
+  public function setRoles(array $roles)
+  {
+    $this->roles = $roles;
+    return $this;
+  }
+
+  public function getRoles()
+  {
+    return $this->roles;
+  }
 }
