9f4b63b76a0173bb6c164819d08df44e086f8fba
ragulan77 <ragulan.charlesm@gmail.com>
2013-12-23 00:28:50 +0100

    new profile form type added

---
+++ src/Ath/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ath\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+class ProfileFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        // add your custom field
+        $builder->add('username');
+        $builder->add('firstname');
+        $builder->add('lastname');
+        $builder->add('birthdate');
+        $builder->add('address');
+        $builder->add('zipcode');
+        $builder->add('city');
+        $builder->add('phone');
+    }
+
+    public function getName()
+    {
+        return 'ath_user_profile';
+    }
+}
