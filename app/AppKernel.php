89cdb81d059aaf4fb49b17e5bc7d3d44b957471d
ragulan77 <ragulan.charlesm@gmail.com>
2014-01-11 00:28:12 +0100

    adding of fosjsrouting

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Ath\PageBundle\AthPageBundle(),
         	new Ath\CoursBundle\AthCoursBundle(),
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
