5485a355cbe4b7e21d8d7ca815a86f7fcfc65229
ragulan77 <ragulan.charlesm@gmail.com>
2014-01-14 15:15:10 +0100

    angular controller is more complete

--- src/Ath/ExerciseBundle/Resources/angularjs/js/controllers.js
+++ src/Ath/ExerciseBundle/Resources/angularjs/js/controllers.js
@@ -26,11 +26,35 @@ var myAppCtrls = angular.module('myApp.controllers', []);
 
   myAppCtrls.controller('ExerciseCtrl', ['$scope', '$http', '$route', '$routeParams', '$compile', function($scope, $http, $route, $routeParams, $compile) {
     $scope.exerciseData = null;
+    $scope.answers = [];
+    $scope.isRightAnswer = false;
     $http.get(Routing.generate('ath_exercise_get_data', {id: $routeParams.exerciseId})).success(function(data){
         $scope.exerciseData = data;
       });
 
     $scope.templateUrl = Routing.generate('ath_exercise_get_subject_view', {id: $routeParams.exerciseId});
+
+    $scope.oneAnswer = function(answer){
+      $scope.answers = [answer];
+    };
+
+    $scope.checkAnswers = function(user_answers){
+      var data = {answers: JSON.stringify(user_answers)};
+
+      var route = Routing.generate('ath_exercise_check_answers', {id: $routeParams.exerciseId});
+      var config = {
+        headers: {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+      }
+
+      $http.post(route, data, config).success(function(resp){
+        if(resp == "true"){
+          $scope.isRightAnswer = true;
+        }
+        else{
+          $scope.isRightAnswer = false;
+        }
+      });
+    };
 
   }]);
 
