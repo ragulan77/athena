78511477f8e5992ea195d73c5752d252c86cd3d6
ragulan77 <ragulan.charlesm@gmail.com>
2014-01-14 22:31:31 +0100

    Exception thrown when a service is not found

--- src/Ath/ExerciseBundle/Exercises/AthExerciseManager.php
+++ src/Ath/ExerciseBundle/Exercises/AthExerciseManager.php
@@ -30,13 +30,21 @@ class AthExerciseManager
   public function getSubjectTemplate(ExerciseFile $exercise)
   {
     $serviceName = $exercise->getType();
-    return $this->container->getParameter('exercise_services_list')[$serviceName]['subject_template'];
+    $exercise_services_list = $this->container->getParameter('exercise_services_list');
+    if(!array_key_exists($serviceName, $exercise_services_list))
+      throw new \Exception('Cette page n\'existe pas');
+
+    return $exercise_services_list[$serviceName]['subject_template'];
   }
 
   public function getCreateTemplate($type)
   {
-    $serviceName = $type;
-    return $this->container->getParameter('exercise_services_list')[$serviceName]['create_template'];
+      $serviceName = $type;
+      $exercise_services_list = $this->container->getParameter('exercise_services_list');
+      if(!array_key_exists($serviceName, $exercise_services_list))
+        throw new \Exception('Cette page n\'existe pas');
+
+      return $exercise_services_list[$serviceName]['create_template'];
   }
 
   /* retourne un tableau de chaine de caractères */
