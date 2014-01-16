a4c6abfb00b70eee5c47666fc900f832800ee5a0
ragulan77 <ragulan.charlesm@gmail.com>
2014-01-17 00:16:19 +0100

    useless commented code

--- src/Ath/ExerciseBundle/Controller/ExerciseController.php
+++ src/Ath/ExerciseBundle/Controller/ExerciseController.php
@@ -13,21 +13,6 @@ class ExerciseController extends Controller
     */
     public function indexAction()
     {
-      /*$repository = $this->getDoctrine()
-                   ->getManager()
-                   ->getRepository('AthExerciseBundle:ExerciseFile');
-
-      $exercise = $repository->find(2);
-      $serializer = $this->get('serializer');
-      var_dump($serializer->serialize($exercise, "json"));
-      $exerciseServiceManager = $this->get('ath_exercise.manager');
-      $exerciseService = $exerciseServiceManager->getRightExerciseService($exercise);
-
-      $exerciseService = $this->get($exerciseService);
-      $subject = $exerciseService->getSubject($exercise);
-      $answers = array(0);
-      $test = $this->getSubjectAction($exercise); */
-
       return $this->render('AthExerciseBundle:Default:index.html.twig');
     }
 
