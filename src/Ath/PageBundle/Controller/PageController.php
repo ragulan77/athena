5453d9f3f27e22133bef05bdf26bcfed2f024026
ragulan77 <ragulan.charlesm@gmail.com>
2014-01-24 23:26:39 +0100

    menu changed

--- src/Ath/PageBundle/Controller/PageController.php
+++ src/Ath/PageBundle/Controller/PageController.php
@@ -25,24 +25,10 @@ class PageController extends Controller
     	//récupérer ses classes et ses matières
     	if( $securityContext->isGranted('ROLE_STUDENT') ){
     		$listeClasses = $user->getClasse(); //classe unique
-    		$disciplines = $em->getRepository('AthCoursBundle:Discipline')->getDisciplinesByClasseId($listeClasses->getId());
-    		$listeMatieresParClasse = array(
-    			$listeClasses->getName() => $disciplines
-    		);
     	}elseif( $securityContext->isGranted('ROLE_PROFESSOR') ) {
     		$listeClasses = $user->getClasses();
-    		$listeMatieresParClasse=array();
-    		foreach ($listeClasses as $classe){
-    			$disciplines = $em->getRepository('AthCoursBundle:Discipline')->getDisciplinesByClasseId($classe->getId());
-    			$listeMatieresParClasse[$classe->getName()] = $disciplines;
-    		}
     	}elseif( $securityContext->isGranted('ROLE_ADMINISTRATOR') ){
     		$listeClasses = $em->getRepository('AthUserBundle:Classe')->findAll();
-    		$listeMatieresParClasse=array();
-    		foreach ($listeClasses as $classe){
-    			$disciplines = $em->getRepository('AthCoursBundle:Discipline')->getDisciplinesByClasseId($classe->getId());
-    			$listeMatieresParClasse[$classe->getName()] = $disciplines;
-    		}
     	}
 
     	return $this->render(
