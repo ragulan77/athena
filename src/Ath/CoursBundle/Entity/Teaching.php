639ad2d72ee0082cd23a617bffe1db736d81f563
ragulan77 <ragulan.charlesm@gmail.com>
2014-01-25 10:55:03 +0100

    getter setter for teaching

--- src/Ath/CoursBundle/Entity/Teaching.php
+++ src/Ath/CoursBundle/Entity/Teaching.php
@@ -43,4 +43,73 @@ class Teaching
     {
         return $this->id;
     }
+
+    /**
+     * Set professor
+     *
+     * @param \Ath\UserBundle\Entity\Professor $professor
+     * @return Teaching
+     */
+    public function setProfessor(\Ath\UserBundle\Entity\Professor $professor)
+    {
+        $this->professor = $professor;
+    
+        return $this;
+    }
+
+    /**
+     * Get professor
+     *
+     * @return \Ath\UserBundle\Entity\Professor 
+     */
+    public function getProfessor()
+    {
+        return $this->professor;
+    }
+
+    /**
+     * Set classe
+     *
+     * @param \Ath\UserBundle\Entity\Classe $classe
+     * @return Teaching
+     */
+    public function setClasse(\Ath\UserBundle\Entity\Classe $classe)
+    {
+        $this->classe = $classe;
+    
+        return $this;
+    }
+
+    /**
+     * Get classe
+     *
+     * @return \Ath\UserBundle\Entity\Classe 
+     */
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+
+    /**
+     * Set discipline
+     *
+     * @param \Ath\CoursBundle\Entity\Discipline $discipline
+     * @return Teaching
+     */
+    public function setDiscipline(\Ath\CoursBundle\Entity\Discipline $discipline)
+    {
+        $this->discipline = $discipline;
+    
+        return $this;
+    }
+
+    /**
+     * Get discipline
+     *
+     * @return \Ath\CoursBundle\Entity\Discipline 
+     */
+    public function getDiscipline()
+    {
+        return $this->discipline;
+    }
 }
