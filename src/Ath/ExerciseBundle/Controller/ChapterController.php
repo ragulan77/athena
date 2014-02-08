13b874659a30a9e678fd0c2466924029e0a3e986
ragulan77 <ragulan.charlesm@gmail.com>
2014-02-08 12:06:58 +0100

    fixing wrong path of Level and Chapter

--- src/Ath/ExerciseBundle/Controller/ChapterController.php
+++ src/Ath/ExerciseBundle/Controller/ChapterController.php
@@ -4,14 +4,14 @@ namespace Ath\ExerciseBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-use Ath\ExerciseBundle\Entity\Chapter;
+use Ath\CoursBundle\Entity\Chapter;
 
 class ChapterController extends Controller
 {
    public function getChaptersAction()
    {
       $em = $this->getDoctrine()->getManager();
-      $repo = $em->getRepository('AthExerciseBundle:Chapter');
+      $repo = $em->getRepository('AthCoursBundle:Chapter');
       $chapters = $repo->findAll();
       $serializer = $this->get('serializer');
 
